6b86a055a333c9f585fa42d673d72d31762e25e7
Your Name <you@example.com>
2024-11-15 12:02:19 +0800

    commit 0: helloword act 3

---
+++ dags/helloworld.py
@@ -0,0 +1,17 @@
+from airflow.decorators import dag, task
+
+
+@dag(schedule="@daily", start_date=datetime(2021, 12, 1), catchup=False)
+def taskflow():
+
+    @task
+    def helloWorld():
+        print("Hello World")
+
+    helloWorld()
+
+
+taskflow()
+
+
+          
