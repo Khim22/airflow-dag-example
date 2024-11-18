33bb7a40024ce0e739352939467019b022f65cd7
Your Name <you@example.com>
2024-11-18 22:40:53 +0800

    try nested 2

--- dags/nested/helloworld2.py
+++ dags/nested/helloworld2.py
@@ -3,12 +3,12 @@ import time
 from airflow.decorators import dag, task
 
 @dag(schedule="@daily", start_date=datetime(2021, 12, 1), catchup=False)
-def helloWorld():
+def helloWorld2():
     @task
-    def helloWorld():
+    def helloWorld2():
         time.sleep(2)
         print("Hello World 2")
 
-    helloWorld()
+    helloWorld2()
 
-helloWorld()
+helloWorld2()
